d545ad3e9406f43efadc429854714a75b0ccc874
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-06-12 19:20:34 +0530

    Update okta.main.tf

--- okta.main.tf
+++ okta.main.tf
@@ -79,9 +79,7 @@ resource "okta_app_oauth" "k8s_oidc" {
   token_endpoint_auth_method = "none" # this sets the client authentication to PKCE
   grant_types = [
     "authorization_code",
-    "implicit",
-    "id_token",
-    "token"
+    #"implicit",
   ]
   response_types = ["code"]
   redirect_uris = [
