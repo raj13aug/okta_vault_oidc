c151bc09264a58c1c3045e6c86c80baa110f4c1d
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-06-12 19:39:02 +0530

    Create required_providers.tf

---
+++ required_providers.tf
@@ -0,0 +1,16 @@
+
+# Enable and configure the Okta provider
+terraform {
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "okta" {
+  org_name  = var.org_name
+  base_url  = var.base_url
+  api_token = var.api_token
+}
